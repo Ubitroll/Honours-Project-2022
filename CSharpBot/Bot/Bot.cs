2be5b839c82856a296a11d8f70351a51a2a612bc
Ubitroll <stephencartner@hotmail.co.uk>
2022-03-28 11:49:04 +0100

    Updated bot to set up first version of states.

--- CSharpBot/Bot/Bot.cs
+++ CSharpBot/Bot/Bot.cs
@@ -140,14 +140,112 @@ namespace Bot
         {
             // Output code here
 
+            Vector3 leftMostTarget;
+            Vector3 rightMostTarget;
+
+            // Retrieve relavent data to make a save
+            if (agent.Team == 1)
+            {
+                rightMostTarget = new Vector3(800f, 5120f, 321f);
+                leftMostTarget = new Vector3(-800f, 5120f, 321f);
+            }
+            else
+            {
+                rightMostTarget = new Vector3(-800f, -5120f, 321f);
+                leftMostTarget = new Vector3(800f, -5120f, 321f);
+            }
+
+            Vector3 carLocation = packet.Players[agent.Index].Physics.Location;
+            Vector3 ballLocation = packet.Ball.Physics.Location;
+
+            // Get directional vectors
+            Vector3 carToBall = ballLocation - carLocation;
+            Vector3 carToBallDirection = Vector3.Normalize(carToBall);
+
+            Vector3 ballToLeftTargetDirection = Vector3.Normalize(leftMostTarget - ballLocation);
+            Vector3 ballToRightTargetDirection = Vector3.Normalize(rightMostTarget - ballLocation);
+
+            // Use clamp method to get the direction of approach
+            Vector3 directionOfAppraoch = Clamp2D(carToBallDirection, ballToLeftTargetDirection, ballToRightTargetDirection);
+
+            // Offset ball location by radius of the ball in direction of where we want to approach from to find where we want to hit the ball from.
+            Vector3 offsetBallLocation = ballLocation - (directionOfAppraoch * 92.75f);
+
+            // Circle ball until direction matches close approx of direction of approach
+            int sideOfApproachDirection = System.Math.Sign(Vector3.Dot(Vector3.Cross(directionOfAppraoch, new Vector3(0, 0, 1)), ballLocation - carLocation));
+
+            Vector3 carToBallPerpendicular = Vector3.Normalize(Vector3.Cross(carToBall, new Vector3(0, 0, sideOfApproachDirection)));
+            float adjustment = System.Math.Abs(AngleBetween(Flatten(carToBall), Flatten(directionOfAppraoch))) * 2560;
+            Vector3 finalTarget = offsetBallLocation + (carToBallPerpendicular * adjustment);
+
+            // Find the relative between the target and the car current location then steer towards it.
+            Orientation carRotation = packet.Players[agent.Index].Physics.Rotation;
+            Vector3 relativeFinalTarget = Orientation.RelativeLocation(carLocation, finalTarget, carRotation);
+
+            // Steer towards final target
+            if (relativeFinalTarget.Y > 0)
+            {
+                steer = -1;
+            }
+            else
+            {
+                steer = 1;
+            }
+
+            // If the ball is close to the car
+            if (Vector3.Distance(carLocation, ballLocation) < 60)
+            {
+                // If the ball is in the air above the car try jump for it
+                if (ballLocation.Z > carLocation.Z)
+                {
+                    jump = true;
+                }
+                else
+                {
+                    jump = false;
+                }
+            }
 
 
-            // If the AI has picked up a boost exit state
-            if (Vector3.Distance(packet.Ball.Physics.Location, fieldInfo.Goals[agent.Index].Location) > 100)
+            // Method to find angle between two vectors
+            float AngleBetween(Vector3 vector1, Vector3 vector2)
+            {
+                float dotProduct = Vector3.Dot(vector1, vector2);
+                double magnitude = System.Math.Sqrt((vector1.X * vector1.X) + (vector1.Y * vector1.Y) + (vector1.Z * vector1.Z)) * System.Math.Sqrt((vector2.X * vector2.X) + (vector2.Y * vector2.Y) + (vector2.Z * vector2.Z));
+                double angleRadians = System.Math.Acos(dotProduct / magnitude);
+
+                return (float)angleRadians;
+            }
+
+            // Method to flatten vector - sets z value to 0
+            Vector3 Flatten(Vector3 vector)
+            {
+                Vector3 flattenedVector = new Vector3(vector.X, vector.Y, 0);
+
+                return flattenedVector;
+            }
+
+            // Method to change the carToBallDirection vector so it lies between the two target points
+            Vector3 Clamp2D(Vector3 direction, Vector3 start, Vector3 end)
+            {
+                bool isRight = Vector3.Dot(direction, Vector3.Cross(end, new Vector3(0, 0, -1))) < 0;
+                bool isLeft = Vector3.Dot(direction, Vector3.Cross(start, new Vector3(0, 0, -1))) > 0;
+
+                if ((Vector3.Dot(end, Vector3.Cross(start, new Vector3(0, 0, -1))) > 0) ? (isRight && isLeft) : (isRight || isLeft))
+                    return direction;
+
+                if (Vector3.Dot(start, direction) < Vector3.Dot(end, direction))
+                    return end;
+
+                return start;
+            }
+
+            // If the AI hit the ball then swap state.
+            if (Vector3.Distance(ballLocation, fieldInfo.Goals[agent.Index].Location) < 300)
                 return null;
             // else follow controls to get to nearest boost
             else
-                return new Controller { Throttle = 1, Steer = steer };
+                return new Controller { Steer = steer, Throttle = 1, Boost = true, Jump = jump };
 
         }
     }
@@ -167,7 +265,7 @@ namespace Bot
 
         public override bool IsViable(Bot agent, Packet packet, FieldInfo fieldInfo)
         {
-            return packet.Players[agent.Index].Boost < 80;
+            return Vector3.Distance(packet.Players[agent.Index].Physics.Location, packet.Ball.Physics.Location) < 400;
         }
 
         public override Controller? GetOutput(Bot agent, Packet packet, FieldInfo fieldInfo)
@@ -212,6 +310,33 @@ namespace Bot
             float adjustment = System.Math.Abs(AngleBetween(Flatten(carToBall), Flatten(directionOfAppraoch))) * 2560;
             Vector3 finalTarget = offsetBallLocation + (carToBallPerpendicular * adjustment);
 
+            // Find the relative between the target and the car current location then steer towards it.
+            Orientation carRotation = packet.Players[agent.Index].Physics.Rotation;
+            Vector3 relativeFinalTarget = Orientation.RelativeLocation(carLocation, finalTarget, carRotation);
+
+            // Steer towards final target
+            if (relativeFinalTarget.Y > 0)
+            {
+                steer = -1;
+            }
+            else
+            {
+                steer = 1;
+            }
+
+            // If the ball is close to the car
+            if (Vector3.Distance(carLocation, ballLocation) < 60)
+            {
+                // If the ball is in the air above the car try jump for it
+                if (ballLocation.Z > carLocation.Z)
+                {
+                    jump = true;
+                }
+                else
+                {
+                    jump = false;
+                }
+            }
 
 
             // Method to find angle between two vectors
@@ -248,11 +373,11 @@ namespace Bot
             }
 
             // If the AI hit the ball then swap state.
-            if (Vector3.Distance(ballLocation, carLocation) <= 94)
+            if (Vector3.Distance(ballLocation, carLocation) <= 100)
                 return null;
             // else follow controls to get to nearest boost
             else
-                return new Controller { };
+                return new Controller { Steer = steer, Throttle = 1, Boost = true, Jump = jump };
         }
     }
 
@@ -272,14 +397,30 @@ namespace Bot
         // Just hollow since state can only be called when no other state are viable.
         public override bool IsViable(Bot agent, Packet packet, FieldInfo fieldInfo)
         {
-            return packet.Players[agent.Index].Boost < 80;
+            return false;
         }
 
         public override Controller? GetOutput(Bot agent, Packet packet, FieldInfo fieldInfo)
         {
             // Output code here
 
+            // Turn car towards ball
+            Vector3 carLocation = packet.Players[agent.Index].Physics.Location;
+            Vector3 ballLocation = packet.Ball.Physics.Location;
+            Orientation carRotation = packet.Players[agent.Index].Physics.Rotation;
 
+            Vector3 ballRelative = Orientation.RelativeLocation(carLocation, ballLocation, carRotation);
+
+            if (ballRelative.Y > 0)
+            {
+                steer = -1;
+            }
+            else
+            {
+                steer = 1;
+            }
+
+            return new Controller {Throttle = 1 ,Steer = steer, Boost = true };
         }
     }
 
