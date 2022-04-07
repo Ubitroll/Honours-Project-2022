80c7134d95da7a9092d9fd67a77c132b5fda43a7
Ubitroll <stephencartner@hotmail.co.uk>
2022-04-07 09:02:20 +0100

    Disabled Bot State print out for testing

--- CSharpBot/Bot/StateHandler.cs
+++ CSharpBot/Bot/StateHandler.cs
@@ -75,12 +75,12 @@ namespace Bot
             {
                 // Render the state information to the screen. (Useful for debugging to make sure bot is in right state)
                 // Allows for multiple agents to be displayed at once if multiple AI are running simultaneously.
-                float renderTextPosY = 100 + 30 * agent.Index;
-                string renderText = $"{GetStateName(currentState!)}";
-                Vector2 upperLeft = new Vector2(10, renderTextPosY);
-                System.Drawing.Color colour = agent.Team == 0 ? System.Drawing.Color.DarkBlue : System.Drawing.Color.DarkOrange;
-                agent.Renderer.DrawRectangle2D(colour, upperLeft, renderText.Length * 20, 30, true);
-                agent.Renderer.DrawString2D(renderText, System.Drawing.Color.White, upperLeft, 2, 2);
+                //float renderTextPosY = 100 + 30 * agent.Index;
+                //string renderText = $"{GetStateName(currentState!)}";
+                //Vector2 upperLeft = new Vector2(10, renderTextPosY);
+                //System.Drawing.Color colour = agent.Team == 0 ? System.Drawing.Color.DarkBlue : System.Drawing.Color.DarkOrange;
+                //agent.Renderer.DrawRectangle2D(colour, upperLeft, renderText.Length * 20, 30, true);
+                //agent.Renderer.DrawString2D(renderText, System.Drawing.Color.White, upperLeft, 2, 2);
                 return stateOutput.Value;
             }
                 
